2b918231eb068dab636f10ddbf1206556d8a1a68
Chandra Kiran Senapati <kiran-chandra.senapati@hpe.com>
2022-04-09 15:50:24 +0530

    ToDo Project

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return <h1>Todos:</h1>
+}
+
+// class Header extends React.Component {
+//     render() {
+//         return (
+//             <h1>Todos: </h1>
+//         )
+//     }
+// }
+
+export default Header;
